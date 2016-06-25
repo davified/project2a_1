d7a5ef99ea910dea676bd65b0c79df84e1c3517f
David Tan <davified@gmail.com>
2016-06-25 15:39:36 +0800

    users validation and authentication completed. functional

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 6 }
+  before_save { self.email = email.downcase }
 end
